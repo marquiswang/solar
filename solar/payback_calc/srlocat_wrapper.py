04014916f6e95f354cd15a16d52e6b048cc5b392
mawang <mawang@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-06 17:53:37 +0000

    Relative path in srlocat_wrapper doesn't work so well... This probably has a better fix
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@13 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/payback_calc/srlocat_wrapper.py
+++ solar/payback_calc/srlocat_wrapper.py
@@ -1,6 +1,6 @@
 from subprocess import Popen,PIPE
-
-
+import os
+import sys
 
 def parse_line(srline):
     splitline = srline.split()
@@ -17,7 +17,8 @@ def srlocat(lat, long, year, month):
         outputs a list of tuples of the form 
             (daily average sunlight, sunlight weighted average of zenith angle)
     """
-    command = ["./srlocat"]
+    command = ["payback_calc/srlocat"]
+    print os.getcwd()+command[0]
     args = [str(i) for i in [lat, long, year, month]]
     srlocat_process = Popen(command+args, stdout=PIPE)
     srlocat_output = \
