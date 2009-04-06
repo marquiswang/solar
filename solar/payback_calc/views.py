f697fe333ac898a3e6d1fd7a49b17036855346ee
sjust <sjust@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-06 08:46:50 +0000

    updates to the wrapper and views.py.  The template for the main form should go into templates/index.html
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@9 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/payback_calc/views.py
+++ solar/payback_calc/views.py
@@ -1,13 +1,21 @@
-# Create your views here.
-
 from django.template import Context, loader
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
+    """
+        This is a stub.  When this function is called, the main page of the
+        payback calculator should load.  templates/index.html should have
+        the form.
+    """
     return render_to_response('index.html', {})
 
 
 def calc_payback(request):
+    """
+        This is a stub for rendering the response to the form in index.html.
+        calc_payback should handle handing the information from the response
+        to the relevant logic and should assemble the response into a response
+        using the response.html template at templates/response.html.
+    """
     return render_to_response('response.html', {})
-
