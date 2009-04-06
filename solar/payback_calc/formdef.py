74dd53434b1e0fc5ef165724118c16bf500823f8
mawang <mawang@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-06 17:52:42 +0000

    Added labels to the formdef
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@12 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/payback_calc/formdef.py
+++ solar/payback_calc/formdef.py
@@ -6,7 +6,13 @@ class SolarForm(forms.Form):
     panel_size = forms.FloatField()
     installation_angle = forms.FloatField()
     price_per_panel = forms.FloatField()
-    panel_efficiency = forms.FloatField()
+    panel_rating = forms.FloatField()
+
+    number_of_panels.label = "Number of panels"
+    panel_size.label = "Panel size (square centimeters)"
+    installation_angle.label = "Installation angle"
+    price_per_panel.label = "Price per panel"
+    panel_rating.label = "Panel rating (Kw per square meter)"
 
     # coords
     latitude = forms.FloatField()
