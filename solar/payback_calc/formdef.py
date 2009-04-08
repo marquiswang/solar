2b2de3f0ab5c58a4efce3991b1a176679472d9ef
mawang <mawang@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-08 05:55:51 +0000

    Uses total system cost and peak output instead of individual panel ratings and costs
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@21 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/payback_calc/formdef.py
+++ solar/payback_calc/formdef.py
@@ -2,22 +2,26 @@ from django import forms
 
 class SolarForm(forms.Form):
     # first part
-    number_of_panels = forms.IntegerField()
-    number_of_panels.label = "Number of panels"
+    # number_of_panels = forms.IntegerField()
+    # number_of_panels.label = "Number of panels"
+    # 
+    # panel_size = forms.FloatField()
+    # panel_size.label = "Panel size (square centimeters)"
+    # 
+    # installation_angle = forms.FloatField()
+    # installation_angle.label = "Installation angle"
+    # 
+    # price_per_panel = forms.FloatField()
+    # price_per_panel.label = "Price per panel"
+    # 
+    # panel_rating = forms.FloatField()
+    # panel_rating.label = "Panel rating (Kw per square meter)"
 
-    panel_size = forms.FloatField()
-    panel_size.label = "Panel size (square centimeters)"
+    peak_power_output = forms.FloatField()
+    peak_power_output.label = "Peak power output (W)"
     
-    installation_angle = forms.FloatField()
-    installation_angle.label = "Installation angle"
-
-    price_per_panel = forms.FloatField()
-    price_per_panel.label = "Price per panel"
-    
-    panel_rating = forms.FloatField()
-    panel_rating.label = "Panel rating (Kw per square meter)"
-
-
+    installation_price = forms.FloatField()
+    installation_price.label = "Installation price"
 
     # coords
     latitude = forms.FloatField()
--- solar/payback_calc/views.py
+++ solar/payback_calc/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render_to_response
 from formdef import SolarForm
 from solar.payback_calc.srlocat_wrapper import srlocat
 
-import math
-
 def index(request):
     """
         This is a stub.  When this function is called, the main page of the
@@ -16,7 +14,6 @@ def index(request):
     return render_to_response('index.html', {'form': form})
 
 
-
 def calc_payback(request):
     """
         This is a stub for rendering the response to the form in index.html.
@@ -24,28 +21,29 @@ def calc_payback(request):
         to the relevant logic and should assemble the response into a response
         using the response.html template at templates/response.html.
     """
-    if request.method != 'POST': # If the form has been submitted...
+     # Check to make sure a form has been submitted...
+    if request.method != 'POST':
         return render_to_response('error.html', {})
 
-    form = SolarForm(request.POST) # A form bound to the POST data
+	# Bind form to the POST data
+    form = SolarForm(request.POST)
 
     if (not form.is_valid()): # All validation rules pass
         return render_to_response('error.html', {})
 
+    form.clean()
     
+    # Calculate upcoming energy output
     monthly_energy_output = []
-    # changed to form.cleaned_data rather than form.data
+
     for m in range(0, 12):
         month = m+1
-        insolation = srlocat(form.cleaned_data['latitude'], \
-                             form.cleaned_data['longitude'], 2009, month)
+        insolation = srlocat(form.data['latitude'], \
+                             form.data['longitude'], 2009, month)
 
         months_energy_output = 0
         for day in insolation:
-            days_power_output = form.cleaned_data['panel_size']*\
-                                form.cleaned_data['panel_rating']*day[0]/1000*\
-                                math.cos(form.cleaned_data['installation_angle']*\
-                                math.pi/180)/10000 
+            days_power_output = form.data['peak_power_output']*day[0]/1000  
             days_energy_output = days_power_output*24
             months_energy_output += days_energy_output
         monthly_energy_output.append(months_energy_output)
@@ -54,9 +52,14 @@ def calc_payback(request):
     for month in monthly_energy_output:
         years_energy_output += month
 
-    output_data = form.cleaned_data.copy()
+    output_data = form.data.copy()
     output_data["monthly_energy_output"] = monthly_energy_output
     output_data["years_energy_output"] = years_energy_output
+    
+     # All validation rules pass
+    if form.is_valid():
+        return render_to_response('response.html', output_data)
     return render_to_response('response.html', output_data)
 
+    
 
