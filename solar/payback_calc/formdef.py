af2f969e74eadd76b4a920c167423dae4b3ecef2
mawang <mawang@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-08 07:33:13 +0000

    Minro changes: Changed some form fields
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@26 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/payback_calc/formdef.py
+++ solar/payback_calc/formdef.py
@@ -1,15 +1,19 @@
 from django import forms
 
 class SystemForm(forms.Form):
-    peak_power_output = forms.FloatField()
+    peak_power_output = forms.IntegerField()
     peak_power_output.label = "Peak power output (W)"
     
     installation_price = forms.FloatField()
     installation_price.label = "Installation price"
 
 class LocationForm(forms.Form):
-    latitude = forms.FloatField()
-    longitude = forms.FloatField()
+    latitude = forms.DecimalField(required=False, max_digits=7, decimal_places=4)
+    longitude = forms.DecimalField(required=False, max_digits=7, decimal_places=4)
+    city = forms.CharField(required=False)
+    state = forms.CharField(required=False)
+    zip_code = forms.CharField(required=False)
+    zip_code.label = "ZIP"
 
 class CostsForm(forms.Form):
     avg_price = forms.FloatField()
