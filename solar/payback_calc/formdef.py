b4382f51763a3b6a779772de683b126096e01b80
mawang <mawang@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-08 18:12:49 +0000

    Added choices for location form
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@27 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/payback_calc/formdef.py
+++ solar/payback_calc/formdef.py
@@ -11,8 +11,61 @@ class LocationForm(forms.Form):
     latitude = forms.DecimalField(required=False, max_digits=7, decimal_places=4)
     longitude = forms.DecimalField(required=False, max_digits=7, decimal_places=4)
     city = forms.CharField(required=False)
-    state = forms.CharField(required=False)
-    zip_code = forms.CharField(required=False)
+    state = forms.ChoiceField(required=False)
+    state.choices =  \
+        [('AL',	'Alabama'),
+        ('AK',	'Alaska'),
+        ('AZ',	'Arizona'),
+        ('AR',	'Arkansas'),
+        ('CA',	'California'),
+        ('CO',	'Colorado'),
+        ('CT',	'Connecticut'),
+        ('DE',	'Delaware'),
+        ('DC',	'District of Columbia'),
+        ('FL',	'Florida'),
+        ('GA',	'Georgia'),
+        ('HI',	'Hawaii'),
+        ('ID',	'Idaho'),
+        ('IL',	'Illinois'),
+        ('IN',	'Indiana'),
+        ('IA',	'Iowa'),
+        ('KS',	'Kansas'),
+        ('KY',	'Kentucky'),
+        ('LA',	'Louisiana'),
+        ('ME',	'Maine'),
+        ('MD',	'Maryland'),
+        ('MA',	'Massachusetts'),
+        ('MI',	'Michigan'),
+        ('MN',	'Minnesota'),
+        ('MS',	'Mississippi'),
+        ('MO',	'Missouri'),
+        ('MT',	'Montana'),
+        ('NE',	'Nebraska'),
+        ('NV',	'Nevada'),
+        ('NH',	'New Hampshire'),
+        ('NJ',	'New Jersey'),
+        ('NM',	'New Mexico'),
+        ('NY',	'New York'),
+        ('NC',	'North Carolina'),
+        ('ND',	'North Dakota'),
+        ('OH',	'Ohio'),
+        ('OK',	'Oklahoma'),
+        ('OR',	'Oregon'),
+        ('PA',	'Pennsylvania'),
+        ('RI',	'Rhode Island'),
+        ('SC',	'South Carolina'),
+        ('SD',	'South Dakota'),
+        ('TN',	'Tennessee'),
+        ('TX',	'Texas'),
+        ('UT',	'Utah'),
+        ('VT',	'Vermont'),
+        ('VA',	'Virginia'),
+        ('WA',	'Washington'),
+        ('WV',	'West Virginia'),
+        ('WI',	'Wisconsin'),
+        ('WY',	'Wyoming')]
+    
+    zip_code = forms.CharField(required=False, max_length=5)
     zip_code.label = "ZIP"
 
 class CostsForm(forms.Form):
