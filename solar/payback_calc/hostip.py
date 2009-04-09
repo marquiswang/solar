24955762261730f9e856dcd6ac25ea564f8d5a65
mawang <mawang@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-09 00:54:13 +0000

    Minor update: hostip.py didn't zip correctly
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@32 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/payback_calc/hostip.py
+++ solar/payback_calc/hostip.py
@@ -5,12 +5,13 @@ import string
 def hostip(ip_str):
     """
     hostip: Wrapper for ip geolocation database
-    Uses database from http://hostip.info
+    Uses ip database from http://hostip.info and publicly available zip code database pulled from
+    http://scripts.ringsworld.com/calculators/zipcode-1.1.0/
     
     @ip_str: IPv4 address in "aaa.bbb.ccc.ddd" form.
     
-    Outputs a tuple in (city, state, lat, lng) form.
-    Return ("", "", None, None) if nothing is found.
+    Outputs a tuple in (city, state, Ip lat, lng) form.
+    Return ("", "", None, None, None) if nothing is found.
     """
     a,b,c,d = ip_str.split(".")
     conn = MySQLdb.connect (host = 'localhost',
@@ -20,13 +21,17 @@ def hostip(ip_str):
     cursor = conn.cursor()
     cursor.execute("SELECT name, state, lat, lng FROM cityByCountry WHERE (city, country) = (select city, country from ip4_"+a+" where b = "+b+" and c = "+c+")")
     if cursor.rowcount == 0:
-        return ("", "", None, None)
+        return ("", "", None, None, None)
     name, state, lat, lng = cursor.fetchone()
     city = ",".join(urllib.unquote(name).split(',')[:-1])
     state = urllib.unquote(name).split(', ')[-1].strip()
     
     cursor.execute("SELECT zip_code FROM zip_code WHERE city = '"+string.upper(city)+"' AND state_prefix = '"+state+"'");
-    zip_code = str(cursor.fetchone()[0]).rjust(5, "0")
+    
+    zip_code = None
+    if cursor.rowcount > 0:
+        zip_code = str(cursor.fetchone()[0]).rjust(5, "0")
+        
     return (city, state, zip_code, lat, lng)
     
-    
+
