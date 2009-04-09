3ab1ad7048464a4cc50913159b7495b986dfce31
dyakovlev <dyakovlev@305149e5-7d4b-415f-9477-61f394bc3cda>
2009-04-09 04:24:32 +0000

    added the css file and probably made it servable
    git-svn-id: https://svn.cs.hmc.edu/svn/solar2009/trunk@38 305149e5-7d4b-415f-9477-61f394bc3cda

--- solar/urls.py
+++ solar/urls.py
@@ -7,7 +7,10 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     (r'^$', 'solar.payback_calc.views.index'),
-    (r'^calc_payback/', 'solar.payback_calc.views.calc_payback')
+    (r'^calc_payback/', 'solar.payback_calc.views.calc_payback'),
+    #(r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/day/solar2009/trunk/solar/templates/payback_calc'})
+
+    
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
